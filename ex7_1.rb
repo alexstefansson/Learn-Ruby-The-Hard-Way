5a2a25d0f0d5f5985bdcb9a9d285b07ef328563b
Alex Stefansson <alexstefansson@hotmail.com>
2014-04-08 14:08:44 -0700

    Learn Ruby The Hard Way - Exercise 7
    Lots of printing.

---
+++ ex7_1.rb
@@ -0,0 +1,33 @@
+# Learn Ruby The Hard Way - Exercise 7
+
+# prints the string
+puts "Mary had a little lamb."
+# prints string, adds snow as a string variable
+puts "Its fleece was white as %s." % 'snow'
+# prints string
+puts "And everywhere that Mary went."
+# adds declared number of "."
+puts "." * 10 # what'd that do?
+
+# loads string characters into end# variables
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "b"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# notice how we are using print instead of puts here. change it to puts
+# and see what happens.
+# puts and print are equivalent except that there is a line break between cheese and burger when using puts instead of print
+puts end1 + end2 + end3 + end4 + end5 + end6
+puts end7 +end8 +end9 +end10 + end11 + end12
+
+# this is just a polite use of the terminal, try removing it
+puts
